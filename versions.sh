25b40077b585e0f821240eef37ca2bc9d8ce5c42
Carles San Agustin <carles.sanagustin@scrm.eu>
2020-05-05 11:09:07 +0200

    updated version kubectl

--- versions.sh
+++ versions.sh
@@ -13,7 +13,7 @@ oc version | head -1
 # terraform
 terraform --version | head -1
 # kubectl
-echo -n "kubectl: " && kubectl version -o json | jq '.clientVersion.gitVersion'
+echo -n "kubectl: " && kubectl version --client=true -o json | jq '.clientVersion.gitVersion'
 #kubectl version --short=true 2>/dev/null | head -1 | cut -d' ' -f3
 # aws
 pip list 2>/dev/null|grep awscli
